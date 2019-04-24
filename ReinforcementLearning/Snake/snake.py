860b5ed1d63168804d13ad53e525244bb343a10e
KrisNguyen135 <nguyenminhquan135@gmail.com>
2019-04-23 20:48:54 -0400

    updated snake in rl

--- ReinforcementLearning/Snake/snake.py
+++ ReinforcementLearning/Snake/snake.py
@@ -5,11 +5,12 @@ import time
 
 
 class Game:
-    def __init__(self, show=False):
+    def __init__(self, auto=None, show=False):
+        self.auto = auto
         self.show = show
 
         self.food = vector(0, 0)
-        self.snake = [vector(10, 0)]
+        self.snake = [vector(10, 0), vector(20, 0)]
         self.aim = vector(0, -10)
 
     def change(self, x, y):
@@ -45,9 +46,39 @@ class Game:
 
         square(self.food.x, self.food.y, 9, 'green')
         turtle.update()
-        #turtle.ontimer(self.move, 100)
+
+        # Collect stats and feed to AI
+        if self.auto is not None:
+            delta_x = head.x - self.food.x
+            delta_y = head.y - self.food.y
+            direction = self.get_direction()
+            move = self.auto(delta_x, delta_y, direction)
+            if move == 0:
+                self.change(0, 10)
+            elif move == 1:
+                self.change(-10, 0)
+            elif move == 2:
+                self.change(0, -10)
+            elif move == 3:
+                self.change(10, 0)
 
         return True
+
+    def get_direction(self):
+        # 0: up, 1: left, 2: down, 3: right
+        head = self.snake[-1]
+        neck = self.snake[-2]
+
+        if head.x == neck.x:  # moving vertically
+            if head.y > neck.y:
+                return 0
+
+            return 2
+
+        if head.x < neck.x:
+            return 3
+
+        return 1
 
     def run(self):
         if self.show:
@@ -66,8 +97,6 @@ class Game:
                 time.sleep(0.1)
 
             keep_moving = self.move()
-
-        #turtle.done()
 
         return len(self.snake)
 
--- ReinforcementLearning/Snake/snake_solver.py
+++ ReinforcementLearning/Snake/snake_solver.py
@@ -1,6 +1,45 @@
 from ReinforcementLearning.Snake.snake import Game
 # from snake import Game
 
+import os
+import numpy as np
+import random
+
+
+class SparseSolver:
+    file = 'output/sparse.csv'
+
+    def __init__(self):
+        if os.path.isfile(SparseSolver.file):
+            self.q_table = np.genfromtxt(
+                SparseSolver.file,
+                delimiter=','
+            )
+        else:
+            self.q_table = np.zeros((38, 38, 4, 4), dtype=int)  # each delta // 10
+
+        self.training = False
+
+    def make_move(self, delta_x, delta_y, direction):
+        move = np.argmax(self.q_table[delta_x, delta_y, direction])
+        if self.training:
+            self.temp_sequence.append((delta_x, delta_y, direction, move))
+
+        return move
+
+    def train(self, num_rounds):
+        print('Started training...')
+
+        self.training = True
+        for round in range(num_rounds):
+            print()
+            print('=' * 40)
+            print(f'Round {round}:')
+
+            self.temp_sequence = []
+
+            game = Game(show=False, auto=self.make_move)
+
 
 if __name__ == '__main__':
     game = Game(show=True)
